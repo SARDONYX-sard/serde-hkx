e7de7d986dec7c6da309d2761ff86611ef906eb1
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-07-03 01:14:33 +0900

    perf(ser_bytes): organize write count

--- serde_hkx/src/bytes/ser/trait_impls/classnames_writer.rs
+++ serde_hkx/src/bytes/ser/trait_impls/classnames_writer.rs
@@ -29,31 +29,25 @@ impl ClassNamesWriter for Cursor<Vec<u8>> {
     ) -> io::Result<HashMap<&'static str, u32>>
     where
         O: ByteOrder,
-        K: core::fmt::Debug,
         V: HavokClass,
     {
         let classnames_section_start = self.position() as u32;
 
-        let mut class_map = HashMap::new();
-
-        // Four class information that for some reason is always present in the `__classnames__` section.
-        // Not sure where this is used.
+        // These classes are meta-information, such as C++ class signatures, and are always considered
+        // to exist because they are already defined in the SDK.
         self.write_u32::<O>(0x75585ef6)?;
-        self.write_u8(0x09)?;
-        self.write_all(b"hkClass\0")?;
+        self.write_all(b"\x09hkClass\0")?;
 
         self.write_u32::<O>(0x5C7EA4C2)?;
-        self.write_u8(0x09)?;
-        self.write_all(b"hkClassMember\0")?;
+        self.write_all(b"\x09hkClassMember\0")?;
 
         self.write_u32::<O>(0x8A3609CF)?;
-        self.write_u8(0x09)?;
-        self.write_all(b"hkClassEnum\0")?;
+        self.write_all(b"\x09hkClassEnum\0")?;
 
         self.write_u32::<O>(0xCE6F8A6C)?;
-        self.write_u8(0x09)?;
-        self.write_all(b"hkClassEnumItem\0")?;
+        self.write_all(b"\x09hkClassEnumItem\0")?;
 
+        let mut class_map = HashMap::new(); // To write `virtual_fixup`
         for (_, class) in classes.iter() {
             self.write_u32::<O>(class.signature().into())?;
             self.write_u8(0x09)?;
