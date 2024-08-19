fbab2328f64139e334c4e67858cfcde42e24fa3b
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-08-19 12:13:09 +0900

    perf(sort): remove redundant loop for `bytes` sort

--- serde_hkx/src/sort.rs
+++ serde_hkx/src/sort.rs
@@ -1,5 +1,6 @@
 //! Provides a method that can be used to sort bytes and XML to serialize.
 use crate::errors::ser::Error as SerError;
+use havok_serde::HavokClass;
 use indexmap::IndexMap;
 
 /// Trait that provides a method that can be used to sort bytes and XML to serialize
@@ -31,32 +32,56 @@ pub trait HavokSort {
 
 impl<V> HavokSort for IndexMap<usize, V>
 where
-    V: havok_serde::HavokClass,
+    V: HavokClass,
 {
     type Error = SerError;
 
     fn sort_for_bytes(&mut self) {
+        fn collect_deps<V>(
+            classes: &mut IndexMap<usize, V>,
+            key: usize,
+            sorted_keys: &mut Vec<usize>,
+        ) where
+            V: HavokClass,
+        {
+            if sorted_keys.contains(&key) {
+                return;
+            }
+
+            let current_index = match classes.get_index_of(&key) {
+                Some(index) => index,
+                None => return, // Return if the key doesn't exist.
+            };
+
+            sorted_keys.push(key);
+            classes.swap_indices(sorted_keys.len().saturating_sub(1), current_index);
+
+            if let Some(class) = classes.get(&key) {
+                let deps = class.deps_indexes();
+
+                #[cfg(feature = "tracing")]
+                tracing::debug!("index = {key}, deps_indexes = {deps:?}");
+
+                for dep_key in deps {
+                    collect_deps(classes, dep_key, sorted_keys);
+                }
+            }
+        }
+
         if self.is_empty() {
             return;
         }
 
-        let mut sorted_keys = Vec::new();
-        if let Some((&root_key, root_class)) =
-            self.keys().min().and_then(|min| self.get_key_value(min))
-        {
-            collect_deps(self, root_key, root_class, &mut sorted_keys);
-        }
+        let &root_key = match self.keys().min() {
+            Some(min) => min,
+            None => return,
+        };
+
+        let mut sorted_keys = Vec::with_capacity(self.len());
+        collect_deps(self, root_key, &mut sorted_keys);
 
         #[cfg(feature = "tracing")]
-        tracing::debug!("sorted_keys = {sorted_keys:?}");
-        let mut sorted_classes = IndexMap::with_capacity(self.len());
-        for index in &sorted_keys {
-            if let Some(class) = self.swap_remove(index) {
-                sorted_classes.insert(*index, class);
-            }
-        }
-
-        *self = sorted_classes;
+        tracing::debug!("sorted_keys = {:?}", sorted_keys);
     }
 
     fn sort_for_xml(&mut self) -> Result<usize, Self::Error> {
@@ -87,13 +112,14 @@ where
         }
 
         let mut sorted_keys = Vec::new();
-        let mut top_ptr = None;
-        if let Some((&root_key, root_class)) =
-            self.keys().min().and_then(|min| self.get_key_value(min))
-        {
-            top_ptr = Some(root_key);
-            collect_deps(self, root_key, root_class, &mut sorted_keys);
-        }
+        let (&root_key, root_class) = self
+            .keys()
+            .min()
+            .and_then(|min| self.get_key_value(min))
+            .ok_or(SerError::Message {
+                msg: "Missing top pointer.".to_owned(),
+            })?;
+        collect_deps(self, root_key, root_class, &mut sorted_keys);
 
         #[cfg(feature = "tracing")]
         tracing::debug!("sorted_keys = {sorted_keys:?}");
@@ -104,38 +130,6 @@ where
             }
         }
         *self = sorted_classes;
-
-        if let Some(top_ptr) = top_ptr {
-            Ok(top_ptr)
-        } else {
-            Err(SerError::Message {
-                msg: "Missing top pointer.".to_owned(),
-            })
-        }
-    }
-}
-
-/// Create an acyclic directed graph from the order of fields in the root to the tail branch (class of dependencies).
-fn collect_deps<V>(
-    classes: &indexmap::IndexMap<usize, V>,
-    key: usize,
-    class: &V,
-    sorted: &mut Vec<usize>,
-) where
-    V: havok_serde::HavokClass,
-{
-    if sorted.contains(&key) {
-        return;
-    }
-
-    sorted.push(key);
-    let deps = class.deps_indexes();
-    #[cfg(feature = "tracing")]
-    tracing::debug!("index = {key}, deps_indexes = {deps:?}");
-
-    for dep_key in deps {
-        if let Some(dep_class) = classes.get(&dep_key) {
-            collect_deps(classes, dep_key, dep_class, sorted);
-        }
+        Ok(root_key)
     }
 }
