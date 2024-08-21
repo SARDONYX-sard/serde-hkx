a6c69990fafb6c05c4a6706db7183a258dae3f74
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-08-22 03:16:33 +0900

    chore(license): add missing license displays

--- crates/havok_serde/src/de/seed.rs
+++ crates/havok_serde/src/de/seed.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// The following code was written by modifying serde ver. 1.0.202.
+// See: https://github.com/serde-rs/serde/commit/58b3af4c2915c3ae789778a11f3b7a468c1cec17
+//
+// And serde holds the same license as Rust. https://github.com/rust-lang/rust/pull/43498
 use crate::de::{Deserialize, DeserializeSeed, Deserializer};
 
 /// A DeserializeSeed helper for implementing deserialize_in_place Visitors.
