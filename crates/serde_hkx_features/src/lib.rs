038b3e83594e13a53c6adf63fde22a4c184582e7
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-09-18 10:36:33 +0900

    style: sort import

--- crates/serde_hkx_features/src/lib.rs
+++ crates/serde_hkx_features/src/lib.rs
@@ -3,7 +3,7 @@ pub mod diff;
 pub mod dump;
 pub mod error;
 pub mod read_ext;
-pub mod tree;
 mod toml_err_avoider;
+pub mod tree;
 
 pub(crate) type ClassMap<'a> = indexmap::IndexMap<usize, havok_classes::Classes<'a>>;
