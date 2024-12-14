6baae4908d76158a1d2af1409ac63c8826beb0e0
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2024-12-14 23:02:50 +0900

    fix: fix schema

--- crates/serde_hkx_features/src/types_wrapper.rs
+++ crates/serde_hkx_features/src/types_wrapper.rs
@@ -16,15 +16,15 @@ pub struct ClassPtrMap<'a> {
     /// Path of Json schema
     #[cfg_attr(feature = "json_schema", schemars(rename = "$schema"))]
     #[serde(default = "default_schema_url")]
-    schema: Option<Cow<'a, str>>,
+    schema: Cow<'a, str>,
     #[serde(flatten)]
     #[serde(borrow)]
     classes: IndexMap<Pointer, Classes<'a>>,
 }
 
 #[inline]
-const fn default_schema_url() -> Option<Cow<'static, str>> {
-    Some(Cow::Borrowed("https://raw.githubusercontent.com/SARDONYX-sard/serde-hkx/refs/tags/0.7.0/assets/serde_hkx_schema.json"))
+const fn default_schema_url() -> Cow<'static, str> {
+    Cow::Borrowed("https://raw.githubusercontent.com/SARDONYX-sard/serde-hkx/refs/tags/0.7.0/assets/serde_hkx_schema.json")
 }
 
 impl<'a> ClassPtrMap<'a> {
