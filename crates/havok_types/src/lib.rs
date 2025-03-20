24f4a17e62801f2fccbf67ed0f90ad8cf886f7db
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2025-03-20 20:18:44 +0900

    feat: add types

--- crates/havok_types/src/lib.rs
+++ crates/havok_types/src/lib.rs
@@ -27,3 +27,73 @@ mod lib {
 
 /// Unicode null
 pub const NULL_STR: &str = "\u{2400}";
+
+macro_rules! create_enum {
+    ($($name:ident => $type:ty),*) => {
+        $(
+            #[cfg_attr(feature = "json_schema", derive(schemars::JsonSchema))]
+            #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+            #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, parse_display::Display)]
+            #[display("{0}")]
+            pub enum $name<'a> {
+                Number($type),
+                Var(std::borrow::Cow<'a, str>),
+            }
+
+            impl Default for $name<'_> {
+                #[inline]
+                fn default() -> Self {
+                    Self::Number(Default::default())
+                }
+            }
+        )*
+    };
+}
+
+#[cfg_attr(feature = "json_schema", derive(schemars::JsonSchema))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, parse_display::Display)]
+#[display("{0}")]
+pub enum I64<'a> {
+    Number(i64),
+    Var(std::borrow::Cow<'a, str>),
+}
+impl Default for I64<'_> {
+    #[inline]
+    fn default() -> Self {
+        Self::Number(Default::default())
+    }
+}
+
+impl<'a> I64<'a> {
+    #[allow(clippy::should_implement_trait)]
+    pub fn from_str(s: &'a str) -> Self {
+        s.parse::<i64>()
+            .map_or(Self::Var(std::borrow::Cow::Borrowed(s)), |num| {
+                Self::Number(num)
+            })
+    }
+}
+
+create_enum! [
+    U8 => u8,
+    U16 => u16,
+    U32 => u32,
+    U64 => u64,
+
+    I8 => i8,
+    I16 => i16,
+    I32 => i32
+    // I64 => i64
+];
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn array_type() {
+        let array: [U8; 5] = core::array::from_fn(|i| U8::Number(i as u8));
+        println!("{:?}", array);
+    }
+}
